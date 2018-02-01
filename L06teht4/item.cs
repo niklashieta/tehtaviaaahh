71b64d8dafdc430dce49675bd98d62982aa9dcb5
Niklas <Niklas@NIKLAS-PC>
2018-02-01 13:17:58 +0200

    teht1-5

---
+++ L06teht4/item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L06teht4
+{
+    class item
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public item() { }
+        public item(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+}
---
+++ L06teht4/lappari.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L06teht4
+{
+    class lappari : item
+    {
+        public string CPU;
+            public string GPU;
+            public string Akku;
+            public double Nayttores;
+    }
+}
